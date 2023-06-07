e461f68b9177790ae683917d2172324faac39ce9
ani-ruddha <aniruddhadas919@gmail.com>
2023-06-07 17:02:52 +0530

    AT1 FirstCommit

---
+++ DemoJenkis/src/test/java/testCase/Tc2Test.java
@@ -0,0 +1,14 @@
+package testCase;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2Test {
+	
+	@Test
+	public void m1()
+	{
+		Reporter.log("Tc2 Passed",true);
+	}
+
+}
